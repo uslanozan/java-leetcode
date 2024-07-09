15d0e8dd6f7ae22de07f611692fd3ea6c7abb2dc
Ozan Uslan <143701893+uslanozan@users.noreply.github.com>
2024-07-09 15:16:26 +0300

    Create Palindrom.java

---
+++ LeetCode/Palindrom.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+//Given an integer x, return true if x is a palindrome , and false otherwise.
+//Follow up: Could you solve it without converting the integer to a string?
+
+import java.util.Stack;
+
+public class Palindrom {
+
+    public static boolean isPalindrome(int x) {
+        //We don't need to check if x is int or long etc.
+
+        int origin=x;
+        if (x < 0)
+            return false;
+
+        int reverse=0;
+        while (x>0){
+            reverse=reverse*10+x%10;
+            x = x/10;
+        }
+
+        return reverse == origin;
+
+    }
+
+
+    public static void main(String[] args) {
+        int a=989;
+        System.out.println(isPalindrome(a));
+
+    }
+}
